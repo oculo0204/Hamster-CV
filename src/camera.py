d4631e63738fe1e5afe4580964b161c12a69bbd5
Seowon Chang <96184457+oculo0204@users.noreply.github.com>
2026-03-26 00:41:32 +0900

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -6,7 +6,7 @@ import cv2
 
 class Camera:
     def __init__(self, camera_index=0, width= 640, height=480):
-        self.cap = cv2.VideoCapture(camera_index)
+        self.cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
